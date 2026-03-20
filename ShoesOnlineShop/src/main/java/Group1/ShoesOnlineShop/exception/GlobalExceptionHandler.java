b025bd2b833e4ea6fab574d67a842601d44aa01c
buitrunghieucitizen <buitrunghieucuber@gmail.com>
2026-03-20 03:49:37 +0100

    Update after update database

--- ShoesOnlineShop/src/main/java/Group1/ShoesOnlineShop/exception/GlobalExceptionHandler.java
+++ ShoesOnlineShop/src/main/java/Group1/ShoesOnlineShop/exception/GlobalExceptionHandler.java
@@ -12,11 +12,11 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public String handleMaxSizeException(MaxUploadSizeExceededException exc, RedirectAttributes redirectAttributes, HttpServletRequest request) {
-        redirectAttributes.addFlashAttribute("errorMessage", "Error: File size too large! Please upload an image smaller than 5MB.");
+        redirectAttributes.addFlashAttribute("errorMessage", "Error: File size too large! Please upload an image smaller than 20MB.");
         
         // Lấy lại URL trước đó để quay lại đúng form
         String referer = request.getHeader("Referer");
-        if (referer != null) {
+        if (referer != null && !referer.contains("/save")) {
             return "redirect:" + referer;
         }
         return "redirect:/sliders";

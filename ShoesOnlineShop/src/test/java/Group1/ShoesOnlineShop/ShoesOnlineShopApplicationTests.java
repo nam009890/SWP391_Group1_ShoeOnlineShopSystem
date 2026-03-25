67f303ea693624559ddbfd76623ac54ce7e7f7c3
trieuhoangson <sonthhe186811@fpt.edu.vn>
2026-03-26 00:09:37 +0700

    test

--- ShoesOnlineShop/src/test/java/Group1/ShoesOnlineShop/ShoesOnlineShopApplicationTests.java
+++ ShoesOnlineShop/src/test/java/Group1/ShoesOnlineShop/ShoesOnlineShopApplicationTests.java
@@ -1,13 +1,13 @@
-package Group1.ShoesOnlineShop;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ShoesOnlineShopApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package Group1.ShoesOnlineShop;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ShoesOnlineShopApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
